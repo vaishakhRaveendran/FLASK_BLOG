05006fb85629174773c3d4adaa3f49adc658ab9d
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-10-30 17:23:30 +0530

    adding the first function

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,9 @@
 from flask import Flask
+#creating a instance of flask class in a variable called the app.
 app = Flask(__name__)
 @app.route("/")
+#Routes are what we type into browsers to go into different websites
+#Route decorators handles all the complicated backend stuff and simply allows us to write a function that returns what is given to the website
+#'/' represents the home page of the website
 def hello_world():
     return "<p>Hello, World!</p>"
