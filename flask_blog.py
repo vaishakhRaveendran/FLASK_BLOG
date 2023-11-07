7040a8980695cdb2430a2c166681c4a05db5f1a9
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-11-07 09:40:56 +0530

    Completed the session 4

--- flask_blog.py
+++ flask_blog.py
@@ -1,10 +1,45 @@
 from flask import Flask, render_template,url_for,flash,redirect
 from forms import RegistrationForm,LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-# Creating an instance of the Flask class and storing it in a variable called 'app'.
+
 app = Flask(__name__)
 app.config['SECRET_KEY']='c2bbbe4aa84532a255b3dde019dcbdb8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 #Secret Keys are required to protect the website from attacks.
+# Creating an instance of the Flask class and storing it in a variable called 'app'.
+db = SQLAlchemy(app)
+app.app_context().push()
+
+#Using sqlAlchemy we can represent the databases as classes.
+#Creating user class for holding the user data
+#Users and posts will be having a one to many relationship
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(20),unique=True,nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20),nullable=False,default='default.jpg')
+    password=db.Column(db.String(60),nullable=False)
+    posts=db.relationship('Post',backref='author',lazy=True)
+    #posts column in user will run a additional query to get all the post created by the user.
+
+    def __repr__(self):
+        return f'User: {self.username},{self.email}'
+
+#Creating a post class for creating and maintaing blog posts
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), unique=True, nullable=False)
+    datePosted=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    content=db.Column(db.Text,nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+
+    def __repr__(self):
+        return f'User: {self.title},{self.datePosted}'
+
+
 
 # Imagine we made a database call and got this list, and now we want to pass it to the website.
 # We can do this by passing 'posts' as an argument to the 'posts' variable in render_template,
